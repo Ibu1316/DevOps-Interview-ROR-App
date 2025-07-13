14e294543391383ff27f293f0a22185a0ca9eced
Ibu1316 <119467257+Ibu1316@users.noreply.github.com>
2025-07-13 18:14:11 +0530

    Update variables.tf

--- infrastructure/variables.tf
+++ infrastructure/variables.tf
@@ -14,15 +14,47 @@ variable "s3_bucket_name" {
   default = "Devops-app-storage-bucket"
 }
 
-variable "ecr_repo_rails" {}
-variable "ecr_repo_nginx" {}
-variable "rds_db_name" {}
-variable "rds_username" {}
-variable "rds_password" {}
-variable "rds_hostname" {}
-variable "lb_endpoint" {}
-variable "rds_port" {
-  default = "5432"
+variable "ecr_repo_rails" {
+  description = "536697233234.dkr.ecr.us-east-1.amazonaws.com/devops/rails-app"
+  type        = string
 }
+
+variable "ecr_repo_nginx" {
+  description = "536697233234.dkr.ecr.us-east-1.amazonaws.com/devops/nginx-proxy"
+  type        = string
+}
+
+variable "rds_db_name" {
+  description = "devdb"
+  type        = string
+}
+
+variable "rds_username" {
+  description = "devuser"
+  type        = string
+}
+
+variable "rds_password" {
+  description = "Nopass@145214"
+  type        = string
+  sensitive   = true
+}
+
+variable "rds_hostname" {
+  description = "RDS endpoint hostname"
+  type        = string
+}
+
+variable "rds_port" {
+  description = "Port number for the RDS PostgreSQL database"
+  type        = number
+  default     = 5432
+}
+
+variable "lb_endpoint" {
+  description = "DNS endpoint of the Load Balancer (without http)"
+  type        = string
+}
+
 
 
