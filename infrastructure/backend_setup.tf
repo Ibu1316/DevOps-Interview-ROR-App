32c8ad0645fe9ea869aedda89d89904beb78112c
Ibu1316 <119467257+Ibu1316@users.noreply.github.com>
2025-07-13 23:12:24 +0530

    Create backend_setup.tf

---
+++ infrastructure/backend_setup.tf
@@ -0,0 +1,36 @@
+# S3 bucket for storing remote state fiel
+resource "aws_s3_bucket" "tf_state" {
+  bucket = "devopss3-ror-app-tfstate-bucket"
+
+  versioning {
+    enabled = true
+  }
+
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        sse_algorithm = "AES256"
+      }
+    }
+  }
+
+  tags = {
+    Name = "Terraform State Bucket"
+  }
+}
+
+# DynamoDB table for state locking
+resource "aws_dynamodb_table" "tf_lock" {
+  name         = "terraform-locks"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "LockID"
+
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+
+  tags = {
+    Name = "Terraform Lock Table"
+  }
+}
