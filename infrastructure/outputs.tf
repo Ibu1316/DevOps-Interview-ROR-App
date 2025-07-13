d7531cff8a25aa8fdac96f98befded81ac1f9c9d
Ibu1316 <119467257+Ibu1316@users.noreply.github.com>
2025-07-13 17:19:11 +0530

    Update outputs.tf

--- infrastructure/outputs.tf
+++ infrastructure/outputs.tf
@@ -5,3 +5,12 @@ output "vpc_id" {
 output "private_subnets" {
   value = module.vpc.private_subnets
 }
+
+output "s3_bucket_name" {
+  value = aws_s3_bucket.app_bucket.id
+}
+
+output "alb_dns_name" {
+  value = aws_lb.this.dns_name
+}
+
