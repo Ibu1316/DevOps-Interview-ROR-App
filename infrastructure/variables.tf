106b39b6705217c0f731c22f5ffcc80eaf7f9a95
Ibu1316 <119467257+Ibu1316@users.noreply.github.com>
2025-07-13 17:14:07 +0530

    Update variables.tf

--- infrastructure/variables.tf
+++ infrastructure/variables.tf
@@ -13,3 +13,12 @@ variable "environment" {
 variable "s3_bucket_name" {
   default = "Devops-app-storage-bucket"
 }
+
+variable "ecr_repo_rails" {}
+variable "ecr_repo_nginx" {}
+variable "rds_db_name" {}
+variable "rds_username" {}
+variable "rds_password" {}
+variable "rds_hostname" {}
+variable "rds_port" {}
+variable "lb_endpoint" {}
