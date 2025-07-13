2410a612de6e421d8ad1670555e73776ba80dff0
Ibu1316 <119467257+Ibu1316@users.noreply.github.com>
2025-07-13 17:53:40 +0530

    Update elb.tf

--- infrastructure/elb.tf
+++ infrastructure/elb.tf
@@ -64,6 +64,6 @@ resource "aws_lb_listener" "this" {
 
   default_action {
     type             = "forward"
-    target_group_arn = aws_lb_target
+    target_group_arn = aws_lb_target_group.this.arn
  }
 }
