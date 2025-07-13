c89c66a80c766ac635b80f7a3a3ef72c3510c59f
Ibu1316 <119467257+Ibu1316@users.noreply.github.com>
2025-07-13 23:35:49 +0530

    Update main.tf

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -1,3 +1,13 @@
 provider "aws" {
   region = var.aws_region
 }
+
+terraform {
+  backend "s3" {
+    bucket         = "devops-ror-app-tfstate-bucket"
+    key            = "global/s3/terraform.tfstate"
+    region         = "us-east-1"
+    dynamodb_table = "terraform-locks"
+    encrypt        = true
+  }
+}
