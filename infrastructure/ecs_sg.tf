4d41346d98769f72b1ef73cd39d18ff50781f451
Ibu1316 <119467257+Ibu1316@users.noreply.github.com>
2025-07-13 17:16:17 +0530

    Create ecs_sg.tf

---
+++ infrastructure/ecs_sg.tf
@@ -0,0 +1,23 @@
+resource "aws_security_group" "ecs_sg" {
+  name        = "${var.app_name}-ecs-sg"
+  description = "Allow ALB to access ECS tasks"
+  vpc_id      = module.vpc.vpc_id
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"] # or restrict to ALB only later
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "${var.app_name}-ecs-sg"
+  }
+}
