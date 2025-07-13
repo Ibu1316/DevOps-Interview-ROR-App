bc74b1f29d419c0081a9fd3887db6fc3dca9cad5
Ibu1316 <119467257+Ibu1316@users.noreply.github.com>
2025-07-13 18:16:15 +0530

    Update outputs.tf

--- infrastructure/outputs.tf
+++ infrastructure/outputs.tf
@@ -14,3 +14,14 @@ output "alb_dns_name" {
   value = aws_lb.this.dns_name
 }
 
+output "rds_hostname" {
+  description = "The endpoint of the RDS PostgreSQL instance"
+  value       = aws_db_instance.this.endpoint
+}
+
+output "lb_endpoint" {
+  description = "The DNS name of the Application Load Balancer"
+  value       = aws_lb.this.dns_name
+}
+
+
