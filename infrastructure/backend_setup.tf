2e2885387144355886577063fb00a42d520610d8
Ibu1316 <119467257+Ibu1316@users.noreply.github.com>
2025-07-15 09:38:54 +0530

    Update backend_setup.tf

--- infrastructure/backend_setup.tf
+++ infrastructure/backend_setup.tf
@@ -1,17 +1,19 @@
-resource "aws_s3_bucket" "tf_state" {
-  bucket = "devops-ror-app-tfstate-bucket"
-  versioning {
-    enabled = true
-  }
+/*
+#resource "aws_s3_bucket" "tf_state" {
+#  bucket = "devops-ror-app-tfstate-bucket"
+#  versioning {
+#    enabled = true
+#  }
 
-  lifecycle {
-    prevent_destroy = false
-  }
+#  lifecycle {
+#    prevent_destroy = false
+#  }
 
-  tags = {
-    Name = "Terraform State Bucket"
-  }
+#  tags = {
+#    Name = "Terraform State Bucket"
+#  }
 }
+*/
 
 # Separate encryption configuration (new recommended way)
 resource "aws_s3_bucket_server_side_encryption_configuration" "tf_state_sse" {
