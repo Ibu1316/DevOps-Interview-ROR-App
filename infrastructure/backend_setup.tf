42005cd543b40a045346bea6ff0fe9ef999cd69d
Ibu1316 <119467257+Ibu1316@users.noreply.github.com>
2025-07-15 09:08:05 +0530

    Update backend_setup.tf

--- infrastructure/backend_setup.tf
+++ infrastructure/backend_setup.tf
@@ -23,19 +23,3 @@ resource "aws_s3_bucket_server_side_encryption_configuration" "tf_state_sse" {
     }
   }
 }
-
-# DynamoDB table for state locking
-resource "aws_dynamodb_table" "tf_lock" {
-  name         = "terraform-locks"
-  billing_mode = "PAY_PER_REQUEST"
-  hash_key     = "LockID"
-
-  attribute {
-    name = "LockID"
-    type = "S"
-  }
-
-  tags = {
-    Name = "Terraform Lock Table"
-  }
-}
